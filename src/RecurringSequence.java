809ff29a9ae1594775c70cc4c73c452fdbdbf7aa
vadigerman <vadigerman@gmail.com>
2018-07-23 14:15:39 +0300

    fix "recurringSequence"

--- src/RecurringSequence.java
+++ src/RecurringSequence.java
@@ -1,25 +1,26 @@
 public class RecurringSequence {
     public static int countRecurringSequence (int[] arr) {
         int number = arr[0];
-        int currentNumber = arr[0];
+        int currentNumber;
         int lengthSequence = 1;
         int currentLengthSequence = 1;
         for(int i = 1; i < arr.length; i++) {
-            if (arr[i] == currentNumber) {
+            currentNumber = arr[i];
+            if (currentNumber == arr[i - 1]) {
                 currentLengthSequence++;
-            }
-            if (currentLengthSequence > lengthSequence) {
-                lengthSequence = currentLengthSequence;
-                number = currentNumber;
+                if (currentLengthSequence > lengthSequence) {
+                    lengthSequence = currentLengthSequence;
+                    number = currentNumber;
+                }
+            } else {
                 currentLengthSequence = 1;
             }
-            currentNumber = arr[i];
         }
         return number;
     }
 
     public static void main(String[] args) {
-        int[] arr = {1, 5, 5, 5, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 5, 5, 5, 5, 5};
+        int[] arr = {1, 5, 5, 5, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 5, 5, 5, 5, 5, 5, 5};
         System.out.println(countRecurringSequence(arr));
     }
 }
