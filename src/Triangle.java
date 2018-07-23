8fe6c72faaffed722a0bf4c2525b24402b5e3dd9
vadigerman <vadigerman@gmail.com>
2018-07-23 13:54:23 +0300

    add "drawTriangle"

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,16 +1,7 @@
 import java.util.Scanner;
 
 public class Triangle {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("enter a base's length: ");
-        int baseLength = sc.nextInt();
-        if (baseLength <= 0) {
-            System.out.println("length must be a positive number");
-            System.exit(0);
-        }
-
+    public static void drawTriangle (int baseLength) {
         int vertexTriangle = (baseLength % 2 == 0) ? 2 : 1;
         for (int i = vertexTriangle; i <= baseLength; i = i + 2) {
             for (int j = 0; j < (baseLength - i) / 2; j++) {
@@ -21,5 +12,15 @@ public class Triangle {
             }
             System.out.println();
         }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a base's length: ");
+        int baseLength = sc.nextInt();
+        if (baseLength <= 0) {
+            System.out.println("length must be a positive number");
+            System.exit(0);
+        }
+        drawTriangle(baseLength);
     }
 }
